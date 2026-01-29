8e9116ef8508075f76fa70338dc7e3ab1ab93d44
Ahmad Yar <ahamdjin34@gmail.com>
2026-01-29 10:35:42 +0500

    commit

--- pages/[[...path]].js
+++ pages/[[...path]].js
@@ -1,17 +1,27 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const INDEX_HTML_PATH = path.join(process.cwd(), "public", "index.html");
-const INDEX_HTML = fs.readFileSync(INDEX_HTML_PATH, "utf8");
+const INDEX_HTML = fs.readFileSync(path.join(process.cwd(), "public", "index.html"), "utf8");
+const GOHIGHLEVEL_HTML = fs.readFileSync(
+  path.join(process.cwd(), "public", "gohighlevel.html"),
+  "utf8",
+);
+
+function normalizeRoute(segments) {
+  if (!Array.isArray(segments) || segments.length === 0) return "/";
+  return `/${segments.join("/")}`.replace(/\/+$/, "");
+}
 
 export default function CatchAllPage() {
   return null;
 }
 
-export async function getServerSideProps({ res }) {
+export async function getServerSideProps({ res, params }) {
+  const route = normalizeRoute(params?.path);
+  const html = route === "/gohighlevel" ? GOHIGHLEVEL_HTML : INDEX_HTML;
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.end(INDEX_HTML);
+  res.end(html);
   return { props: {} };
 }
-
